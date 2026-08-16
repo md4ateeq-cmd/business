ef99498c972e0b7f08fab0fe9bef2acbd0760d8e
muhammed mukhtar ahmed <mukhtarmuhammed011@gmail.com>
2026-08-16 17:18:04 +0530

    basic task 6 to 10

--- task.sql
+++ task.sql
@@ -1,3 +1,22 @@
 use ecom;
--- basic task 1
-select customername,city,orderdate,status from orders
+-- basic task 6 to 10 (mukhtar)
+SELECT *
+FROM orders
+WHERE amount BETWEEN 1000 AND 10000;
+
+SELECT customername,city
+FROM orders
+WHERE city = 'Mumbai' or city = 'bangalore';
+
+SELECT *
+FROM orders
+where orderdate <  2026-08-10;
+
+SELECT customername,city,amount
+FROM orders 
+order by amount desc;
+
+SELECT count(orderdate)
+FROM orders 
+
+
